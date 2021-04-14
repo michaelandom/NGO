3a673a9c47d28b8c2cee703dec4fcda156ce850b
michael andom <michaelandom03@gmail.com>
2021-04-14 07:49:21 +0300

    paginate added to user

--- routers/user.py
+++ routers/user.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, status
 import schemas
 from database import get_db
 from sqlalchemy.orm import Session
-from typing import List
+from fastapi_pagination import Page,add_pagination,paginate
+
 from repository import user
 from oauth2 import get_current_user
 router = APIRouter(
@@ -16,9 +17,9 @@ def create_User(request: schemas.CreateUser, db: Session = Depends(get_db), curr
     return user.create_user(request, db)
 
 
-@router.get("/", response_model=List[schemas.showUser])
+@router.get("/", response_model=Page[schemas.showUser])
 def get_All_User(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    return user.get_all(db)
+    return paginate(user.get_all(db))
 
 
 @router.get("/{id}", response_model=schemas.showUser)
@@ -32,3 +33,4 @@ def delete_Blog(id, db: Session = Depends(get_db), current_user: schemas.User = 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update_Blog(id, request: schemas.User, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     return user.update_user(id, request, db)
+add_pagination(router)
